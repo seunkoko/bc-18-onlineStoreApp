1b3bf50b6a909b726e90e1d2dc03082dbee0e307
seunkoko <seunowonikoko@gmail.com>
2017-01-17 09:55:40 +0100

    Functionalities added to dynamically load the signup and login divs

--- public/layout/login.js
+++ public/layout/login.js
@@ -1,20 +1,32 @@
-  
-  <!DOCTYPE html>
-  <html lang="en"> 
-    <head>
-      <title>My Online Store</title>
-      <meta charset="utf-8">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
-      <link rel="stylesheet" type="text/css" href="/style.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
-      <script type = "text/javascript" src = "../app.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </head>
+	
+	$(document).ready(function() {
 
-    <body style="background-color:#2B488F;">
-      
-    </body>
-  </html>
+		// setting up my database
+		var dbRef = new Firebase('https://online-store-app.firebaseio.com');
+		
+		// handles page layout
+		$( "#signup" ).hide();
+
+		$( "#signupBtn" ).click(function() {
+			console.log("#signupBtn active");
+		});
+
+		$( "#linkSignup" ).click(function() {
+			console.log("#linkSignup active");
+			$( "#login" ).hide('slow');
+			$( "#signup" ).show('slow');
+		});
+
+		$( "#linkLogin" ).click(function() {
+			console.log("#linkLogin active");
+			$( "#signup" ).hide('slow');
+			$( "#login" ).show('slow');
+		});
+
+
+	});
+
+
+
+
+
